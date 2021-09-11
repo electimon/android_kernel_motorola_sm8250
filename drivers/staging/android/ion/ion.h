25daa787506a9c34f6fd1f762bce9bced4eb2351
electimon <electimon@gmail.com>
2021-09-11 10:34:36 +0000

    Revert "ion: dmabuf improve debugfs"
    This reverts commit 3227b6c1ed3378fb48b6f6e67c4d0719c1d94ae4.

--- drivers/staging/android/ion/ion.h
+++ drivers/staging/android/ion/ion.h
@@ -69,8 +69,6 @@
 /* if low watermark of zones have reached, defer the refill in this window */
 #define ION_POOL_REFILL_DEFER_WINDOW_MS	10
 
-#define MAX_CLIENTS_NUM 16
-
 /**
  * struct ion_platform_heap - defines a heap in the given platform
  * @type:	type of the heap from ion_heap_type enum
@@ -146,9 +144,6 @@ struct ion_buffer {
 	struct sg_table *sg_table;
 	struct list_head attachments;
 	struct list_head vmas;
-	pid_t pid;
-	pid_t client_pids[MAX_CLIENTS_NUM];
-	int ref_cnt;
 };
 
 void ion_buffer_destroy(struct ion_buffer *buffer);
